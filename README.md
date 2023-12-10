9fe4942a043e8377c897212048a57cbac08b8a3e
Riya267 <riyacec05@gmail.com>
2023-12-10 20:21:10 +0530

    Categories and Filters Resolvers added

--- README.md
+++ README.md
@@ -1,22 +1,43 @@
-# Ecommerce Backend API Repository
+<h1 align="center">Welcome to ecommerce_backend 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="#" target="_blank">
+    <img alt="License: ISC" src="https://img.shields.io/badge/License-ISC-yellow.svg" />
+  </a>
+  <a href="https://twitter.com/riyacec05" target="_blank">
+    <img alt="Twitter: riyacec05" src="https://img.shields.io/twitter/follow/riyacec05.svg?style=social" />
+  </a>
+</p>
 
-Welcome to the backend repository for our comprehensive ecommerce project. This repository houses a set of APIs that serve as the backbone for client-side functionalities and administrative tasks. These APIs cover a wide range of features, including product information retrieval, user details management, cart operations, and administrative tasks like inventory and store management.
+> Welcome to the backend repository for our comprehensive ecommerce project. This repository houses a set of APIs that serve as the backbone for client-side functionalities and administrative tasks. These APIs cover a wide range of features, including product information retrieval, user details management, cart operations, and administrative tasks like inventory and store management.
 
-## Getting Started
+## Install
 
-### Prerequisites
-
-Make sure you have the following prerequisites installed:
-
-- Node.js: [Download and Install Node.js](https://nodejs.org/)
-- Pnpm: [Install Pnpm](https://pnpm.io/) Install pnpm globally npm i -g pnpm
-- Editor: Install any editor, for example, [Visual Studio Code](https://code.visualstudio.com/)
-
-### Installation
-
-Clone the repository:
-
-```bash
-git clone https://github.com/your-username/ecommerce-backend.git
-cd ecommerce-backend
+```sh
 pnpm install
+```
+
+## Usage
+
+```sh
+pnpm start
+```
+
+## Run tests
+
+```sh
+pnpm test
+```
+
+## Author
+
+👤 **Riya Dhawan**
+
+* Website: https://myportfolio-virid-psi.vercel.app/
+* Twitter: [@riyacec05](https://twitter.com/riyacec05)
+* Github: [@Riya267](https://github.com/Riya267)
+* LinkedIn: [@riya-dhawan-592ab921a](https://linkedin.com/in/riya-dhawan-592ab921a)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,32 +1,62 @@
-import { createProduct } from "./resolvers/createProduct";
-import { deleteProduct } from "./resolvers/deleteProduct";
-import { getAllProducts } from "./resolvers/getAllProducts";
-import { getProduct } from "./resolvers/getProduct";
-import { updateProduct } from "./resolvers/updateProduct";
+import {createProduct, deleteProduct, getAllProducts, getProduct, updateProduct} from './resolvers/product';
+import {getCategories, createCategory, updateCategory, deleteCategory} from './resolvers/category';
+import {getFilters, createFilters, updatefilter, deletefilter} from './resolvers/filter';
 
 export default {
     Query: {
-      getProduct : async (_, args, { context }) => {
-        const product = await getProduct(args, context);
+      getProduct : async (_, args, ) => {
+        const product = await getProduct(args);
         return product;
       },
-      getAllProducts : async (_, args, { context }) => {
-        const products = await getAllProducts(args, context);
+      getAllProducts : async (_, args, ) => {
+        const products = await getAllProducts(args);
         return products;
+      },
+      getCategories : async (_, args, ) => {
+        const categories= await getCategories(args);
+        return categories;
+      },
+      getFilters : async (_, args, ) => {
+        const filters = await getFilters(args);
+        return filters;
       },
     },
     Mutation: {
-      createProduct : async (_, args, { context }) => {
-        const product = await createProduct(args, context);
+      createProduct : async (_, args, ) => {
+        const product = await createProduct(args);
         return product;
       },
-      updateProduct : async (_, args, { context }) => {
-        const product = await updateProduct(args, context);
+      updateProduct : async (_, args, ) => {
+        const product = await updateProduct(args);
         return product;
       },
-      deleteProduct : async (_, args, { context }) => {
-        const product = await deleteProduct(args, context);
+      deleteProduct : async (_, args, ) => {
+        const product = await deleteProduct(args);
         return product;
+      },
+      createCategory : async (_, args, ) => {
+        const category = await createCategory(args);
+        return category;
+      },
+      updateCategory : async (_, args, ) => {
+        const category = await updateCategory(args);
+        return category;
+      },
+      deleteCategory : async (_, args, ) => {
+        const category = await deleteCategory(args);
+        return category;
+      },
+      createFilters : async (_, args, ) => {
+        const filter = await createFilters(args);
+        return filter;
+      },
+      updatefilter : async (_, args, ) => {
+        const filter = await updatefilter(args);
+        return filter;
+      },
+      deletefilter : async (_, args, ) => {
+        const filter = await deletefilter(args);
+        return filter;
       },
     }
 }
